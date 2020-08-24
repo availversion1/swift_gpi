a2ecd1b2b97b3788b46d85a808510d0098080690
Sriram Venkateswaran Iyer <sriramv.iyer@ET-C02YQH9ZLVCF.local>
2020-08-24 22:30:59 +0530

    File watcher changes

--- file_watcher.py
+++ file_watcher.py
@@ -5,26 +5,24 @@ import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, PatternMatchingEventHandler
-
-import requests
+from swift_api_client import send_file_to_server
 
 def on_created(event):
     print(f"hey, {event.src_path} has been created!")
+    send_file_to_server(event.src_path)
+
 
 if __name__ == "__main__":
-
     patterns = "*"
     ignore_patterns = ""
     ignore_directories = False
     case_sensitive = True
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+    my_event_handler.on_created = on_created
 
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = my_event_handler()
+    event_handler = my_event_handler
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
--- swift_api_client.py
+++ swift_api_client.py
@@ -5,6 +5,16 @@ import sys
 
 SWIFT_SERVER = 'http://192.168.29.12:5000/swift_api'
 
+def send_file_to_server(fname):
+    # open file for reading
+    p = open(fname, 'r')
+    contents  = p.read()
+    response = requests.post(SWIFT_SERVER, json=contents)
+
+    print("Status code: ", response.status_code)
+    print("Printing Entire Post Request")
+    print(response.text)
+
 def main():
     print('')
     if len(sys.argv) < 2:
